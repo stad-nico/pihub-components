a6475c514f5f309d754bf59677c6267e56f2bb57
Nicolas Stadler <stadlernicolas26@gmail.com>
2025-03-02 17:42:47 +0100

    fix(data-grid): export `DataGridItem` and extend in documentation

--- projects/pihub/components/data-grid/index.ts
+++ projects/pihub/components/data-grid/index.ts
@@ -4,8 +4,8 @@
  *
  * @author Nicolas Stadler
  *-------------------------------------------------------------------------*/
-import { DataGridComponent } from './data-grid.component';
+import { DataGridComponent, DataGridItem } from './data-grid.component';
 import { ColumnDirective } from './directives/column.directive';
 import { EmptyStateDirective } from './directives/empty-state.directive';
 
-export { ColumnDirective, DataGridComponent, EmptyStateDirective };
+export { ColumnDirective, DataGridComponent, EmptyStateDirective, type DataGridItem };
