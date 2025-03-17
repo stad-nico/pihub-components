385ef971fc8738e5f214448842840970400f46fb
Nicolas Stadler <stadlernicolas26@gmail.com>
2025-03-17 19:40:36 +0100

    feat(*): add release candidate name override

--- release.config.js
+++ release.config.js
@@ -1,6 +1,7 @@
 // @ts-check
 const BRANCH_NAME = process.env.GITHUB_REF_NAME;
 const DEFAULT_BRANCH = process.env.GITHUB_DEFAULT_BRANCH;
+const RC_NAME_OVERRIDE = process.env.RC_NAME_OVERRIDE;
 
 if (!BRANCH_NAME) {
 	throw new Error('Missing GITHUB_REF_NAME');
@@ -10,15 +11,25 @@ if (!DEFAULT_BRANCH) {
 	throw new Error('Missing GITHUB_DEFAULT_BRANCH');
 }
 
+const PRE_RELEASE = BRANCH_NAME !== DEFAULT_BRANCH;
+
+const using = RC_NAME_OVERRIDE ? `RC_NAME_OVERRIDE ${RC_NAME_OVERRIDE}` : `branch name ${BRANCH_NAME.split('/').pop()}`;
+
+PRE_RELEASE && console.log(`Using ${using} for the release candidate`);
+
+const RC_NAME = `${RC_NAME_OVERRIDE || BRANCH_NAME.split('/').pop()}-rc`;
+
+PRE_RELEASE && console.log(`Release candidate name -> ${RC_NAME}`);
+
 /**
  * @type {import('semantic-release').BranchSpec}
  */
-const prereleaseBranch = { name: BRANCH_NAME, prerelease: `${BRANCH_NAME.split('/').pop()}-rc` };
+const prerelease = { name: BRANCH_NAME, channel: RC_NAME, prerelease: RC_NAME };
 
 /**
  * @type {Array<import('semantic-release').BranchSpec>}
  */
-const branches = BRANCH_NAME === DEFAULT_BRANCH ? [DEFAULT_BRANCH] : [DEFAULT_BRANCH, prereleaseBranch];
+const branches = BRANCH_NAME === DEFAULT_BRANCH ? [DEFAULT_BRANCH] : [DEFAULT_BRANCH, prerelease];
 
 /**
  * @type {Array<import('semantic-release').PluginSpec>}
