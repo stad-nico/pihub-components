bc74f994d8e0c2a99afe91694ebbc4314daf0157
Nicolas Stadler <stadlernicolas26@gmail.com>
2025-03-30 17:55:12 +0200

    refactor(sidebar): add library entrypoint

---
+++ projects/pihub/components/sidebar/index.ts
@@ -0,0 +1,10 @@
+/**-------------------------------------------------------------------------
+ * Copyright (c) 2025 - Nicolas Stadler. All rights reserved.
+ * Licensed under the CC BY-NC-SA 4.0 License. See the project root for more information.
+ *
+ * @author Nicolas Stadler
+ *-------------------------------------------------------------------------*/
+import { Divider, SidebarEntry } from './models/sidebar-entry';
+import { SidebarComponent } from './sidebar.component';
+
+export { Divider, SidebarComponent, type SidebarEntry };
