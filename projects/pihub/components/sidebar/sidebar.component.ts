cd3ab9cf5119ed367cf706bd25c1fea91848da18
Nicolas Stadler <stadlernicolas26@gmail.com>
2025-04-05 22:54:11 +0200

    fix(*): add selection state

--- projects/pihub/components/sidebar/sidebar.component.ts
+++ projects/pihub/components/sidebar/sidebar.component.ts
@@ -5,9 +5,10 @@
  * @author Nicolas Stadler
  *-------------------------------------------------------------------------*/
 import { NgTemplateOutlet } from '@angular/common';
-import { ChangeDetectionStrategy, Component, contentChild, inject, input, TemplateRef } from '@angular/core';
-import { Router } from '@angular/router';
+import { ChangeDetectionStrategy, Component, contentChild, inject, input, OnInit, signal, TemplateRef } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 import { IconComponent } from '@pihub/components/icon';
+import { filter } from 'rxjs';
 import { Divider, SidebarEntry } from './models/sidebar-entry';
 
 @Component({
@@ -17,17 +18,60 @@ import { Divider, SidebarEntry } from './models/sidebar-entry';
 	changeDetection: ChangeDetectionStrategy.OnPush,
 	imports: [IconComponent, NgTemplateOutlet],
 })
-export class SidebarComponent {
+export class SidebarComponent implements OnInit {
+	/**
+	 * The entries to display in the sidebar.
+	 */
 	public readonly entries = input<Array<SidebarEntry | Divider>>([]);
 
+	/**
+	 * The entry that is currently selected.
+	 * @internal
+	 */
+	protected readonly selectedEntry = signal<SidebarEntry | null>(null);
+
+	/**
+	 * The optional template to use for the header.
+	 * @internal
+	 */
 	protected readonly headerTemplate = contentChild<TemplateRef<unknown>>('headerTemplate');
 
+	/**
+	 * The optional template to use for the footer.
+	 * @internal
+	 */
 	protected readonly footerTemplate = contentChild<TemplateRef<unknown>>('footerTemplate');
 
+	/**
+	 * The divider entry. Used to separate entries in the sidebar.
+	 * @internal
+	 */
 	protected readonly divider = Divider;
 
+	/**
+	 * The router instance.
+	 * @internal
+	 */
 	protected readonly router = inject(Router);
 
+	public ngOnInit(): void {
+		this.router.events.pipe(filter((event) => event instanceof NavigationEnd)).subscribe((event) => {
+			const entry = this.entries()
+				.filter((entry) => entry !== Divider)
+				.find((entry) => entry.route === event.urlAfterRedirects);
+
+			if (entry) {
+				this.selectedEntry.set(entry);
+			}
+		});
+	}
+
+	/**
+	 * Handle click event on a sidebar entry.
+	 * @internal
+	 *
+	 * @param entry the entry that was clicked
+	 */
 	protected async onEntryClick(entry: SidebarEntry): Promise<void> {
 		await this.router.navigate([entry.route]);
 	}
